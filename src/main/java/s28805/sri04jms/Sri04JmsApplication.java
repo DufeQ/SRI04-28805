a224f1c6b6a14dced28e1ef02768453a20508ab6
dufeq <jakubiak99@gmail.com>
2023-05-19 16:47:26 +0200

    Finnished

--- src/main/java/s28805/sri04jms/Sri04JmsApplication.java
+++ src/main/java/s28805/sri04jms/Sri04JmsApplication.java
@@ -9,7 +9,7 @@ import s28805.sri04jms.receiver.exercise.BolidInfoTopicReceiver2;
 public class Sri04JmsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Sri04JmsApplication.class, args);
+        //SpringApplication.run(Sri04JmsApplication.class, args);
         ConfigurableApplicationContext context = SpringApplication.run(Sri04JmsApplication.class, args);
         BolidInfoTopicReceiver2 bolidInfoTopicReceiver2 = new BolidInfoTopicReceiver2(context);
     }
